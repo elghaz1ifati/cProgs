b8682b73f8a832eeb2aa21eceaa46afa98b4faaa
fati <fatielghazouani@gmail.com>
2025-07-11 21:05:40 +0100

    learn C

--- sumArray.c
+++ sumArray.c
@@ -5,10 +5,11 @@ int main (){
     int Array[]={10,33,40,59};
     int *ptr = Array;
     int sum =0 ;
+
     int i=1;
     while (i<=4){
         sum += *ptr; //Add the current element to sum
-        ptr++; //Move to the next element
+        ptr++; // Move to the next element
         i ++;
     }
     printf("%d",sum);
