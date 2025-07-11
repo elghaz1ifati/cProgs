e98c698297729c466a1052384e7b13b8bd24db34
fati <fatielghazouani@gmail.com>
2025-07-11 21:01:44 +0100

    learnC

--- reverseArray.c
+++ reverseArray.c
@@ -2,7 +2,7 @@
 
 int main(){
     int Array[]={1,2,3,4,5,6,7,8,9,10};
-    int *ptr = Array-1;
+    int *ptr = Array+10-1; //points to the last element of the array
     int i = 0 ;
     while (i<10){
         printf("%d ",*ptr);
